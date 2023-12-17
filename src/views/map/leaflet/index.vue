1df112e4425469424521dc0a1070cdecf15b7f55
han <hannxq@qq.com>
2023-12-17 18:40:36 +0800

    Update index.vue

--- src/views/map/leaflet/index.vue
+++ src/views/map/leaflet/index.vue
@@ -7,6 +7,14 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { onMounted, reactive } from 'vue';
 
+// eslint-disable-next-line no-underscore-dangle
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+})
+
 const me = reactive({ map: null, group: {} })
 const props = defineProps({
   id: { type: String, default: 'map' },
