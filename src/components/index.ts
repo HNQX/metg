edf72f2a903ac0e78ebd43d674dfa12616483acc
han <32836634+HNQX@users.noreply.github.com>
2023-12-17 17:15:22 +0800

    e

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,16 @@
-import { App } from 'vue'
-import { use } from 'echarts/core'
-import { CanvasRenderer } from 'echarts/renderers'
-import { BarChart, LineChart, PieChart, RadarChart } from 'echarts/charts'
-import { GridComponent, TooltipComponent, LegendComponent, DataZoomComponent, GraphicComponent } from 'echarts/components'
-import Chart from './chart/index.vue'
-import Breadcrumb from './breadcrumb/index.vue'
+import { BarChart, LineChart, PieChart, RadarChart } from "echarts/charts";
+import {
+  DataZoomComponent,
+  GraphicComponent,
+  GridComponent,
+  LegendComponent,
+  TooltipComponent,
+} from "echarts/components";
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { App } from "vue";
+import Breadcrumb from "./breadcrumb/index.vue";
+import Chart from "./chart/index.vue";
 // import SvgIcon from './svg-icon/index.vue';
 
 // Manually introduce ECharts modules to reduce packing size
@@ -20,12 +26,12 @@ use([
   LegendComponent,
   DataZoomComponent,
   GraphicComponent,
-])
+]);
 
 export default {
   install(Vue: App) {
-    Vue.component('Chart', Chart)
-    Vue.component('Breadcrumb', Breadcrumb)
+    Vue.component("Chart", Chart);
+    Vue.component("Breadcrumb", Breadcrumb);
     // Vue.component('SvgIcon', SvgIcon);
   },
-}
+};
